a1a9fd326a9b0d891b00891a22f3154185ce0866
BastaMasta <bastamasta3150@proton.me>
2026-01-23 17:06:16 +0530

    smol changes

--- CommsIntegration/main.py
+++ CommsIntegration/main.py
@@ -1,3 +1,13 @@
+import redis
+import paho.mqtt.client as mqtt
+import json
+
+pool =  redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+
+red = redis.Redis(connection_pool=pool)
+
+
+
 def main():
     print("Hello from commsintegration!")
 
