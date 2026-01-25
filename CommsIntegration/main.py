99d81d4d8bd4c1d249a783235182ca035440af02
BastaMasta <bastamasta3150@proton.me>
2026-01-25 14:49:27 +0530

    added sentry to the project to make life easier

--- CommsIntegration/main.py
+++ CommsIntegration/main.py
@@ -2,10 +2,34 @@
 
 import datetime
 import json
+import logging
+import os
 import time
 
 import paho.mqtt.client as mqtt
 import redis
+import sentry_sdk
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Set up sentry for loggin and profiling
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_URL"),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Enable sending logs to Sentry
+    enable_logs=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profile_session_sample_rate to 1.0 to profile 100%
+    # of profile sessions.
+    profile_session_sample_rate=1.0,
+)
+
+logger = logging.getLogger(__name__)
 
 # Defining constants and channels
 jdata = json.load(open("server_data.json", "r", encoding="utf-8"))
@@ -59,6 +83,8 @@ def on_message(client, userdata, msg):
             print("An Error occured on channel esp1!")
             print("On-Board feedback:")
             print(data)
+            logger.error("An Error occured on channel esp1!")
+            logger.error(f"On-Board feedback:\n{msg}")
             time.sleep(1)
             print("Sending incomplete progress feedback to parent process...")
             red.rpush("faliure_stack", str(msg))
@@ -74,5 +100,7 @@ def main():
     print("Hello from commsintegration!")
 
 
+sentry_sdk.profiler.start_profiler()
 if __name__ == "__main__":
     main()
+sentry_sdk.profiler.stop_profiler()
